243dd7dd135065878f3cffaab6e3e4aa1467ebb8
Alper Dortbudak <adortbudak@gmail.com>
2018-10-31 15:39:10 -0500

    improved performance

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const SerialPort = require('serialport');
 const colors = require('colors');
+const moment = require('moment');
 
-const port = new SerialPort('/dev/tty.usbserial-220',{
+
+const port = new SerialPort('/dev/tty.usbserial-A105DBCQ',{
     baudRate: 9600,
     stopBits:1,
     parity: 'none',
@@ -10,10 +12,11 @@ const port = new SerialPort('/dev/tty.usbserial-220',{
 })
 
 var messageLength = 0;
-let incomingBytes = [];
+let incomingBytes = Buffer.alloc(0);
+let pendingBytes = Buffer.alloc(0);
 let message = [];
-let totalRequestsCaptured = 0;
 const gatewaySerialNumber = '12489860';
+const serialDeviceList = ['10362246','10268203']
 
 port.open(function(err) {
     if (err) {
@@ -25,10 +28,59 @@ port.on('open', function(){
     console.log('Port Opened Succesfully');
 });
 
-port.on('data', function(data){
-    data.forEach(element => {
+port.on('data',function(data){
+    let totalLength = incomingBytes.length + data.length;
+    incomingBytes = Buffer.concat([incomingBytes,data]);    
+    
+    //clean up partial data this could happen is serial port is opened and received data while during existing data pushed
+    while (incomingBytes.length > 0  && incomingBytes[0] != 0x72 && incomingBytes[0] != 0X1C && messageLength == 0)
+    {
+        console.log(colors.red("Bytes discarded: ", incomingBytes[0]));
+        incomingBytes = incomingBytes.slice(1);    
+    } 
+
+    if (incomingBytes.length > 0)
+    {        
+        if (pendingBytes.length < messageLength && incomingBytes.length > 0)
+        {
+            let temp = Buffer.alloc(0);
+            temp = incomingBytes.slice(0,messageLength-pendingBytes.length);
+            pendingBytes = Buffer.concat([pendingBytes,temp]);    
+            incomingBytes = incomingBytes.slice(temp.length);
+        }
+        if (pendingBytes.length == 0 && incomingBytes.length > 2)
+        {
+            messageLength = incomingBytes[1] + 1;
+            if (incomingBytes.length <= messageLength)
+            {
+                pendingBytes = Buffer.from(incomingBytes);                
+                incomingBytes = incomingBytes.slice(pendingBytes.length);                
+            }
+        }   
+        if (pendingBytes.length == messageLength)
+        {            
+            process({...pendingBytes});
+         
+            pendingBytes = Buffer.alloc(0);
+            messageLength = 0;
+        }        
+    }
+});
+
+async function process(pendingBytes){
+    if (pendingBytes[0] == 0x72)
+    {                
+        const timeStamp = moment();
+        const deviceSerialNumber = (pendingBytes[3] << 16) + (pendingBytes[4] << 8) + (pendingBytes[5]);
+        console.log("Serial number: ", timeStamp.format("h:mm:ss SSS"), deviceSerialNumber)
+    }
+}
+
+/*port.on('data', function(data){
+    
+     data.forEach(element => {
         incomingBytes.push(element);        
-    });
+    }); 
 
 
     //clean up partial data this could happen is serial port is opened and received data while during existing data pushed
@@ -63,12 +115,18 @@ port.on('data', function(data){
             //const buf = Buffer.from(message);
             if (message[0] == 0x72)
             {
-                const firstHop = (message[7] << 16) + (message[8] << 8) + (message[9]);
-                if (gatewaySerialNumber == firstHop)
-                {
-                    const deviceSerialNumber = (message[3] << 16) + (message[4] << 8) + (message[5]);
-                    const originatorDeviceSerialNumber = (message[3] << 16) + (message[4] << 8) + (message[5]);
                 
+                const deviceSerialNumber = (message[3] << 16) + (message[4] << 8) + (message[5]);
+
+                var found = serialDeviceList.find(function(element)
+                {
+                    return element == deviceSerialNumber;
+                })               
+
+                if (found)
+                {                  
+                    const originatorDeviceSerialNumber = (message[3] << 16) + (message[4] << 8) + (message[5]);
+                    const firstHop = (message[7] << 16) + (message[8] << 8) + (message[9]);
                     const messageClass = message[12];    
 
                     if (messageClass == 0x3E)
@@ -157,11 +215,11 @@ port.on('data', function(data){
                     }
                     console.log(colors.yellow("Status Flag", statusFlag));
             }      
-            console.log(colors.blue("Total Requests Received: ",totalRequestsCaptured));     
-            //console.log(colors.blue(buf));
+            // console.log(colors.blue("Total Requests Received: ",totalRequestsCaptured));     
+            // console.log(colors.blue(buf));
             message = [];      
         }
     }     
-})
+})*/
 
 
